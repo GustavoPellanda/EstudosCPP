800b406c90d4020b75295edf5fa879e9bd936f90
Gustavo Finau Pellanda <gusfipe@gmail.com>
2024-08-03 04:25:11 -0300

    Update Inteligência.cpp

--- Inteligência.cpp
+++ Inteligência.cpp
@@ -39,8 +39,7 @@ protected:
 public:
     Secreto(const int idd = 21): forca(0), Agente(idd){ espias.clear(); }
     virtual ~Secreto(){ espias.clear(); }
-
-    virtual void agir(){ /* FAZER !!! */ }
+    virtual void agir();
     const float getForca() const { return forca; }
     virtual void incluirEspiao(Espiao* p){ 
         if(!p){
@@ -60,8 +59,7 @@ private:
 public:
     Espiao(const int idd = 21): boco(rand()%100 < 90), Agente(idd){ secres.clear(); } // 90% dos espiões começam bocós
     ~Espiao(){ secres.clear(); }
-
-    void agir(){ /* FAZER !!! */ }
+    void agir();
     void incluirSecreto(Secreto* p){
         if(!p){
             cout<< "Erro: Secreto nao existe.";
@@ -80,8 +78,7 @@ private:
 public:
     Duplo(const int idd = 21): pContato(nullptr), Secreto(idd){}
     ~Duplo(){ pContato = nullptr; }
-
-    void agir(){ /* FAZER !!! */ }
+    void agir();
     void incluirEspiao(Espiao* p){
         if(!p){
             cout<< "Erro: Duplo nao existe.";
@@ -90,6 +87,47 @@ public:
         pContato = p;
     }
 };
+
+void Secreto::agir(){
+    if(espias.empty()){
+            cout<< "Erro: Nenhum espiao na lista.";
+        return;
+    } 
+
+    // Determina o número de espiões a serem considerados para remoção
+    int numEspias = static_cast<int>(espias.size());
+    int numRemover = std::min(static_cast<int>(forca), numEspias);
+
+    int numRemovidos = 0;
+    auto iterador = espias.begin();
+    while(iterador != espias.end() && numRemovidos < numRemover){
+        // Remove o espião da lista
+        if((*iterador)->getBoco()){
+            iterador = espias.erase(iterador);
+            cout << "Espião " << (*iterador)->getId() << " neutralizado." << endl;
+        }
+        else ++iterador;
+    }
+}
+
+void Espiao::agir(){
+    auto iterador = secres.begin();
+    while(iterador != secres.end()){
+        if((*iterador)->getForca() > 0.0) (*iterador)->operator--();
+        else{
+            secres.erase(iterador);
+            cout << "Agente secreto " << (*iterador)->getId() << " neutralizado." << endl;
+        }
+        iterador++;
+    }
+}
+
+void Duplo::agir(){
+    if(pContato->getBoco()){
+        pContato->setBoco(false);
+        cout << "Espião " << pContato->getId() << " deixou de ser boco." << endl;
+    }
+}
 
 } 
 
