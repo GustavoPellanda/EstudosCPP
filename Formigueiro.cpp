2b04ae586187a2dd66aafe029e0c0b0be3f1d8d0
Gustavo Finau Pellanda <gusfipe@gmail.com>
2024-08-05 22:49:25 -0300

    Após correção.

--- Formigueiro.cpp
+++ Formigueiro.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -9,12 +10,14 @@ protected:
     static int comidas;
 
 public:
-    Formiga(): comidas(0){}
+    Formiga(){}
     virtual ~Formiga(){}
 
     void operator++(){ comidas++; }
-    const int getComidas() static { return comidas; }
+    static const int getComidas(){ return comidas; }
 };
+
+int Formiga::comidas = 0;
 
 class Operaria : public Formiga {
 private:
@@ -22,22 +25,24 @@ private:
     static int feromonio;
 
 public:
-    Operaria(): exploradora(false), feromonio(0), Formiga(){}
+    Operaria() : exploradora(rand() % 2 == 0){}
     ~Operaria(){}
 
-    void operator++(){ if(exploradora) feromonio++; }
+    void operator++(){ if (exploradora) feromonio++; }
     const bool getExploradora() const { return exploradora; }
-    const int getFeromonio() static { return feromonio; }
+    static const int getFeromonio(){ return feromonio; }
 };
+
+int Operaria::feromonio = 0;
 
 class Formigueiro;
 
 class Rainha : public Formiga {
-private:    
+private:
     Formigueiro* pLar;
 
 public:
-    Rainha(): pLar(nullptr), Formiga(){}
+    Rainha() : pLar(nullptr){}
     ~Rainha(){ pLar = nullptr; }
 
     void setPLar(Formigueiro* pF){ pLar = pF; }
@@ -50,43 +55,31 @@ private:
     vector<Operaria*>::iterator itFormigal;
     Rainha rainha;
 
-protected:
-    Formigueiro(): itFormigal(nullptr), rainha(){ formigal.clear(); }
+public:
+    Formigueiro() : rainha(){ formigal.clear(); }
     ~Formigueiro(){
+        for(auto op : formigal) delete op;
         formigal.clear();
-        itFormigal = nullptr;
     }
 
     void inserir(Operaria* pOp){
         if(!pOp){
-            cout << Erro: Operaria nao existe. << endl;
+            cout << "Erro: Operaria nao existe." << endl;
             return;
         }
         formigal.push_back(pOp);
     }
     
-    void inicio(){ itFormigal == formigal.begin(); }
-    void proximo(){ if(itFormigal) itFormigal++; }
-    Formiga* atual() const { return itFormigal; }
+    void inicio(){ itFormigal = formigal.begin(); }
+    void proximo(){ if(itFormigal != formigal.end()) itFormigal++; }
+    Formiga* atual() const { return (itFormigal != formigal.end()) ? *itFormigal : nullptr; }
 
-    const bool fim() const { 
-        if(itFormigal == formigal.end()) return true;
-        return false; 
-    }
+    const bool fim() const { return itFormigal == formigal.end(); }
 
     const bool vivo() const {
-        int countOperarias = 0;
-        int countComidas = 0;
-
-        Formigueiro.inicio();
-        while(!Formigueiro.fim()){
-            countOperarias++;
-            countComidas += itFormigal->getComidas();
-            Formigueiro.proximo();
-        }
-
-        if((countComidas/10) >=  countOperarias) return true;
-        else return false;
+        int countOperarias = formigal.size();
+        int countComidas = Formiga::getComidas();
+        return (countComidas / 10) >= countOperarias;
     }
 };
 
@@ -95,41 +88,43 @@ private:
     Formigueiro formigueiro;
     int quant_folhas;
 
-protected:
-    Jardim(const int quant_f): quant_folhas(quant_f), formigueiro(){}
+public:
+    Jardim(const int quant_f) : quant_folhas(quant_f){}
     ~Jardim(){}
 
     // Passa pela lista de formigas do formigueiro, incrementando o feromônio para cada folha do jardim:
     void exploracao(){
-        formigaAtual = formigueiro->inicio();
-        while(!formigueiro->fim()){
-            formigaAtual = formigueiro->atual();
-            for(i = 0; i <= quant_folhas; i++) formigaAtual->operator++();
-            formigueiro->proximo();
+        formigueiro.inicio();
+        while(!formigueiro.fim()){
+            Formiga* formigaAtual = formigueiro.atual();
+            for(int i = 0; i < quant_folhas; i++) formigaAtual->operator++();
+            formigueiro.proximo();
         }
     }
 
     // Passa pela lista de formigas do formigueiro, incrementando o valor de comidas equivalente ao valor de feromonios:
     void carreira(){
-        formigaAtual = formigueiro->inicio();
-        while(!formigueiro->fim()){
-            formigaAtual = formigueiro->atual();
-            for(i = 0; i <= formigaAtual->getFeromonio(); i++) formigaAtual->operator++();
-            formigueiro->proximo();
+        formigueiro.inicio();
+        while(!formigueiro.fim()){
+            Formiga* formigaAtual = formigueiro.atual();
+            for(int i = 0; i < Operaria::getFeromonio(); i++) formigaAtual->operator++();
+            formigueiro.proximo();
         }
     }
 
     void executar(){
-        self.exploracao();
-        self.carreira();
-        if(formigueiro.vivo) cout << "Formigueiro continua vivo" << endl;
+        exploracao();
+        carreira();
+        if(formigueiro.vivo()) cout << "Formigueiro continua vivo" << endl;
         else cout << "O Formigueiro morreu :-(" << endl;
     }
 };
 
 int main(){
-    for(int i = 0 i < 20; i++){
+    srand(time(NULL));
+    for(int i = 0; i < 20; i++){
         Jardim jardim(i);
+        jardim.executar();
         cout << endl;
     }
     return 0;
